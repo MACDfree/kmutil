f39c0000cfa2761f1808d411e612144b4ace8612
macdfree <macdfree@163.com>
2020-04-13 01:10:22 +0800

    项目骨架搭建完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="left">
+      <div class="category">分类</div>
+      <div class="tag">标签</div>
+    </div>
+    <div class="middle">
+      <div class="search">搜索</div>
+      <div class="list">列表</div>
+    </div>
+    <div class="main">主</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
+
+<style lang="stylus">
+.home
+  display flex
+.left
+  width 100px
+.middle
+  width 150px
+.main
+  width 100%
+</style>
