21f865fb979704ad03b98848539f4e55b37664e6
macdfree <macdfree@163.com>
2020-04-17 00:58:18 +0800

    完成文档内容编辑页面部分功能

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,6 +8,7 @@
           :expand-on-click-node="false"
           :node-key="'id'"
           :default-expand-all="true"
+          height="100%"
         ></el-tree>
       </div>标签
       <div class="tag">
@@ -27,11 +28,37 @@
       </div>
       <div class="list">
         <ul>
-          <li></li>
+          <li v-for="doc in docs" :key="doc.id" @click="showDoc(doc.id)">{{ doc.title }}</li>
         </ul>
       </div>
     </div>
-    <div class="main">主</div>
+    <div class="main">
+      <div>
+        <el-input v-model="title" placeholder="请输入标题" size="mini" :disabled="!editable"></el-input>
+        <el-button slot="append" size="mini" @click="saveDoc">{{ editable?'保存修改':'开始编辑' }}</el-button>
+        <el-popover placement="top" width="300" v-model="visible">
+          <el-tag
+            :key="tag"
+            v-for="tag in docTags"
+            closable
+            :disable-transitions="false"
+            @close="handleClose(tag)"
+          >{{tag}}</el-tag>
+          <el-input
+            class="input-new-tag"
+            v-if="inputVisible"
+            v-model="inputValue"
+            ref="saveTagInput"
+            size="small"
+            @keyup.enter.native="handleInputConfirm"
+            @blur="handleInputConfirm"
+          ></el-input>
+          <el-button v-else class="button-new-tag" size="small" @click="showInput">+ 新标签</el-button>
+          <el-button slot="reference" size="mini">标签</el-button>
+        </el-popover>
+      </div>
+      <el-tiptap v-model="content" :extensions="extensions" :readonly="!editable" height="100%"></el-tiptap>
+    </div>
   </div>
 </template>
 
@@ -39,6 +66,30 @@
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 import sq3 from 'sqlite3'
+import {
+  ElementTiptap, // 需要的 extensions
+  Doc,
+  Text,
+  Paragraph,
+  Heading,
+  Bold,
+  Underline,
+  Italic,
+  Strike,
+  ListItem,
+  BulletList,
+  OrderedList,
+  TodoItem,
+  TodoList,
+  Table,
+  TableHeader,
+  TableCell,
+  TableRow,
+  FormatClear,
+  History
+} from 'element-tiptap'
+// import element-tiptap 样式
+import 'element-tiptap/lib/index.css'
 // const sharedObject = require('electron').remote.getGlobal('sharedObject')
 const ipcRenderer = require('electron').ipcRenderer
 // const currentPath = sharedObject ? sharedObject.currentPath : null
@@ -64,10 +115,41 @@ export default {
     return {
       directorys: [],
       tags: [],
-      searchStr: ''
+      docs: [],
+      searchStr: '',
+      content: '',
+      title: '',
+      editable: false,
+      visible: false,
+      docTags: ['1', '2'],
+      inputVisible: false,
+      inputValue: '',
+      extensions: [
+        new Doc(),
+        new Text(),
+        new Paragraph(),
+        new Heading({ level: 3 }),
+        new Bold(), // 在气泡菜单中渲染菜单按钮 { bubble: true }
+        new Underline(),
+        new Italic(),
+        new Strike(),
+        new ListItem(),
+        new BulletList(),
+        new OrderedList(),
+        new TodoItem(),
+        new TodoList(),
+        new Table(),
+        new TableHeader(),
+        new TableCell(),
+        new TableRow(),
+        new FormatClear(),
+        new History()
+      ]
     }
   },
-  components: {},
+  components: {
+    'el-tiptap': ElementTiptap
+  },
   mounted() {
     ipcRenderer.on('open-db', (event, arg) => {
       if (arg) {
@@ -99,9 +181,52 @@ export default {
               console.log(err)
             }
           })
+          db.all('select * from km_document order by id desc', function(
+            err,
+            rows
+          ) {
+            if (!err) {
+              rows.forEach(row => {
+                that.docs.push({
+                  id: row.ID,
+                  title: row.TITLE
+                })
+              })
+            } else {
+              console.log(err)
+            }
+          })
         })
       }
     })
+  },
+  methods: {
+    showDoc(docId) {
+      console.log(docId)
+    },
+    saveDoc() {
+      this.editable = !this.editable
+      document.getElementsByClassName(
+        'el-tiptap-editor__menu-bar'
+      )[0].style.display = this.editable ? 'flex' : 'none'
+    },
+    handleClose(tag) {
+      this.docTags.splice(this.docTags.indexOf(tag), 1)
+    },
+    showInput() {
+      this.inputVisible = true
+      this.$nextTick(_ => {
+        this.$refs.saveTagInput.$refs.input.focus()
+      })
+    },
+    handleInputConfirm() {
+      const inputValue = this.inputValue
+      if (inputValue) {
+        this.docTags.push(inputValue)
+      }
+      this.inputVisible = false
+      this.inputValue = ''
+    }
   }
 }
 </script>
@@ -124,6 +249,33 @@ export default {
       cursor pointer
 .middle
   width 300px
+  .list li
+    text-align left
+    cursor pointer
 .main
   width 100%
+  display flex
+  flex-direction column
+  align-items flex-start
+  .el-input
+    width 200px
+.el-tiptap-editor__menu-bar, .el-tiptap-editor>.el-tiptap-editor__content, .el-tiptap-editor__footer
+  border 0
+.el-tiptap-editor__menu-bar:before
+  height 0
+.el-tiptap-editor>.el-tiptap-editor__content
+  padding 10px
+.el-tag
+  margin-right 5px
+  margin-bottom 5px
+.button-new-tag
+  height 32px
+  line-height 30px
+  padding-top 0
+  padding-bottom 0
+  margin-bottom 5px
+.input-new-tag
+  width 90px
+  vertical-align bottom
+  margin-bottom 5px
 </style>
