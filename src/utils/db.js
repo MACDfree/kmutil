b9b4ce8a47ec8744567b328ab3739961b71f2607
MACDfree <macdfree@163.com>
2020-05-17 09:51:11 +0800

    实现info.json的压缩功能

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,8 +1,24 @@
 import global from '../utils/global'
 import low from 'lowdb'
 import FileSync from 'lowdb/adapters/FileSync'
+import pako from 'pako'
+
+function pack(data) {
+  return pako.deflate(JSON.stringify(data), { to: 'string' })
+}
+
+function unpack(data) {
+  if (data.startsWith('{\n')) {
+    return JSON.parse(data)
+  } else {
+    return JSON.parse(pako.inflate(data, { to: 'string' }))
+  }
+}
 
 export default function db() {
-  const adapter = new FileSync(global().currentPath + '\\info.json')
+  const adapter = new FileSync(global().currentPath + '\\info.json', {
+    serialize: pack,
+    deserialize: unpack
+  })
   return low(adapter)
 }
